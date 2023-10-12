c660d0ac3352ccf6617b72c84a6f4f335fb5277b
HolyPeople <holy.people@kakao.com>
2023-10-12 16:57:14 +0900

    add donwload news - #4

--- data-ai/crawler/run.py
+++ data-ai/crawler/run.py
@@ -1,4 +1,5 @@
 from trend_topic import get_topics
+from download_news import get_news
 import json
 
 if __name__ == '__main__':
@@ -7,3 +8,7 @@ if __name__ == '__main__':
     topics = get_topics()
     with open('trend_topics.json', 'w') as f:
         json.dump(topics, f, indent=4, ensure_ascii=False)
+    print('Finish crawling trend topics')
+    print('Start crawling news...')
+    get_news()
+    print('Finish crawling news')
