d77ac0d700a0afc1e4550c1521454ba7b9a72f7e
lminjung99 <lminjung99@konkuk.ac.kr>
2023-10-31 17:06:24 +0900

    message

--- back-end/src/main/java/com/example/test/TestApplication.java
+++ back-end/src/main/java/com/example/test/TestApplication.java
@@ -1,13 +1,31 @@
 package com.example.test;
 
 import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+import com.example.test.config.ServiceKeyConfig;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+
+
+//import com.example.test.config.ServiceKeyConfig;
+@ConfigurationPropertiesScan
 @SpringBootApplication
+@RequiredArgsConstructor
 public class TestApplication {
 
+	private final ServiceKeyConfig serviceKeyConfig;
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
+		System.out.println("configuration property : "+serviceKeyConfig.getServiceKey());
+	}
+
+	@PostConstruct
+	public void init(){
+		System.out.println("configuration property : "+serviceKeyConfig.getServiceKey());
 	}
 
 }
--- back-end/src/main/java/com/example/test/dto/ChatResponse.java
+++ back-end/src/main/java/com/example/test/dto/ChatResponse.java
@@ -5,20 +5,16 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ChatResponse {
 
-	private List<Choice> choices;
+	private String query;
+	private List<Context> contexts;
+	private String answers;
 	
-	@Data
-	@NoArgsConstructor
-	@AllArgsConstructor
-	public static class Choice{
-		private int index;
-		private Message message;
-	}
 	
 }
